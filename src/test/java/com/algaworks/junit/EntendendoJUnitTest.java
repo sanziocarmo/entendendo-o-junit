a9aa66da36728c3d301358b97a129a74c5c64722
Janete <janetega51@gmail.com>
2022-03-24 11:47:04 -0300

    Initial commit

---
+++ src/test/java/com/algaworks/junit/EntendendoJUnitTest.java
@@ -0,0 +1,51 @@
+package com.algaworks.junit;
+
+import org.junit.*;
+
+public class EntendendoJUnitTest {
+
+    @BeforeClass
+    public static void iniciarTestes(){
+       System.out.println(">>> public static void iniciarTestes() <<<");
+    }
+
+    @AfterClass
+    public static void encerrarTestes(){
+        System.out.println(">>> public static void encerrarTestes() <<<");
+    }
+
+    @Before
+    public void iniciarTeste(){
+        System.out.println(">>> public void iniciarTeste() <<<");
+    }
+
+    @After
+    public void encerrarTeste(){
+        System.out.println(">>> public void encerrarTeste() <<<");
+    }
+
+//    @Test
+//    public void testandoAlgo() {
+//        System.out.println(">>> public void testandoAlgo() <<<");
+//    }
+
+    @Test
+    public void testandoAlgo() {
+        String nome = String.format("%s", "Janete");
+
+        Assert.assertEquals("Janete", nome);
+    }
+
+    @Test
+    public void testandoOutraCoisa() {
+        String str = String.format("%s", "");
+
+        Assert.assertTrue(str.isEmpty());
+    }
+
+  //  @Test
+ //   public void testandoOutraCoisa() {
+//        System.out.println(">>> public void testandoOutraCoisa() <<<");
+//    }
+
+}
